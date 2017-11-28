7c95fe75fd9289540eeea730a04ef5a057e33e81
Nitish Sharma <nitish.sharma@keepworks.com>
2017-11-29 01:27:36 +0530

    UI fixes using promises

--- app/assets/javascripts/two_columns/users_stocks.js
+++ app/assets/javascripts/two_columns/users_stocks.js
@@ -1,62 +1,73 @@
 $(function() {
-  var REFRESH_TIMEOUT = 60 * 1000;
+  var REFRESH_TIMEOUT = 500;
 
-  refresh()
-  // setInterval(refresh(), REFRESH_TIMEOUT);
+  refresh();
+  // setInterval(refresh, REFRESH_TIMEOUT);
 
   function refresh() {
     var $headerItems = $('.card-header .stock[data-code="NIFTY"], .stock[data-code="SENSEX"]');
     var $items = $('.user-stock-list li.user-stock-content:not(.user-stock-footer, .hidden)');
 
-    $headerItems.each(function(index) {
-      var $item = $(this);
-
-      $.ajax({
+    var headerItemsPromises = $.map($headerItems, function(item) {
+      return $.ajax({
         type: 'GET',
-        url: '/get_general_price?code=' + $item.data('code'),
-      }).done(function (data) {
-        refreshRealTimeData($item, data)
-      }).fail(function () {
-        jQuery.Deferred().fail.apply(this, arguments);
-      });
+        url: '/get_general_price?code=' + $(item).data('code'),
+      })
     })
 
-    $items.each(function(index) {
-      var $item = $(this);
-
-      $.ajax({
+    var itemsPromises = $.map($items, function(item) {
+      return $.ajax({
         type: 'POST',
-        url: '/get_current_price?users_stock_id=' + $item.data('id') + '&bse_code=' + $item.find('.bse-code').html(),
-      }).done(function (data) {
-        refreshUsersStockData($item, data)
-      }).fail(function () {
-        jQuery.Deferred().fail.apply(this, arguments);
-      });
+        url: '/get_current_price?users_stock_id=' + $(item).data('id') + '&bse_code=' + $(item).find('.bse-code').html(),
+      })
     })
 
-    function refreshRealTimeData($item, data) {
-      $item.find('.amount').html(data.current_price);
-      $item.find('.change .percentage').html(data.change_percentage);
-      refreshCaret($item, data.change_percentage);
-    }
+    $.when.apply($, headerItemsPromises)
+      .then(function() {
+        $.each(arguments, function(index, value) {
+          refreshRealTimeData($($headerItems[index]), value[0])
+        });
+      })
+      .fail(function() {
+        console.log('Failed to refreshRealTimeData')
+      });
 
-    function refreshUsersStockData($item, data) {
-      debugger
-      var $portfolio = $('.stock.total')
+    $.when.apply($, itemsPromises)
+      .then(function() {
+        var currentTotalValue = sumValues(arguments)
+        $.each(arguments, function(index, value) {
+          refreshUsersStockData($($items[index]), value[0], currentTotalValue)
+        });
+      })
+      .fail(function() {
+        console.log('Failed to refreshUsersStockData')
+      });
 
-      $item.find('.cp').html(data.current_price);
-      $item.find('.total').html(data.change_percentage);
-      $item.find('.value').html(data.users_stock_value);
+    console.log('Refreshed')
+  }
 
-      refreshCaret($item, data.change_percentage);
+  function refreshRealTimeData($item, data) {
+    $item.find('.amount').html(data.current_price);
+    $item.find('.change .percentage').html(data.change_percentage);
+    refreshCaret($item, data.change_percentage);
+  }
 
-      $('.user-stock-footer .value').html(sumValues())
-      $('.user-stock-footer .total').html(totalPercentageChange())
+  function refreshUsersStockData($item, data, currentTotalValue) {
+    var $portfolio = $('.stock.total')
 
-      $portfolio.find('.amount').html(sumValues());
-      $portfolio.find('.change .percentage').html(totalPercentageChange());
-      refreshCaret($item, totalPercentageChange());
-    }
+    $item.find('.cp').html(data.current_price);
+    $item.find('.total').html(data.change_percentage);
+    $item.find('.value').html(data.users_stock_value);
+
+    refreshCaret($item, data.change_percentage);
+
+    $('.user-stock-footer .value').html(currentTotalValue)
+    $('.user-stock-footer .total').html(totalPercentageChange(currentTotalValue))
+
+    $portfolio.find('.amount').html(currentTotalValue);
+    $portfolio.find('.change .percentage').html(totalPercentageChange(currentTotalValue));
+
+    refreshCaret($portfolio, totalPercentageChange(currentTotalValue));
   }
 
   function refreshCaret($item, change_percentage) {
@@ -67,19 +78,19 @@ $(function() {
     }
   }
 
-  function sumValues() {
+  function sumValues(arguments) {
     var sum = 0;
 
-    $('li.user-stock-content:not(.hidden, .user-stock-footer) .value').each(function() {
-      sum += parseFloat($(this).html())
-    })
+    $.each(arguments, function(index, value) {
+      sum += value[0].users_stock_value
+    });
 
     return sum;
   }
 
-  function totalPercentageChange() {
+  function totalPercentageChange(currentTotalValue) {
     var totalInvestment = $('.user-stock-footer .investment .amount').html();
-    return ((parseFloat(totalInvestment) / sumValues()) - 1) * 100
+    return ((currentTotalValue / (parseFloat(totalInvestment)) - 1) * 100).toFixed(1)
   }
 })
 
